a6378905e2a4c705365d69e598f0ebcb4f5f3ab4
Andrea Amerigo Aristodemo Gariboldi <andrea.gariboldi@gmail.com>
2014-08-11 18:17:24 +0200

    cap  argv

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+#!/usr/local/bin/node
+
 var argv= require('optimist').argv, 
     _= require('underscore'),
     mw= require('./server/middleware'),
     express= require('express'),
     swim= require('express-swim');
 
-var node= _.defaults(argv,{ host: '127.0.0.1', port: 3000, path: './data', cap: { n: 3, w: 2, r: 2 } }),
+var node= _.defaults(argv,{ host: '127.0.0.1',
+                            port: 3000,
+                            path: './data',
+                             cap: _.defaults(argv.cap || {}, { n: 3, w: 2, r: 2 }) }),
     app = express();
 
 node.string= [node.host,node.port].join(':');
